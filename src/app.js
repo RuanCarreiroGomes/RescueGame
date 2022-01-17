e201e73bc01cace519654b9edd56185ff8ab1695
Ruan Victor <62728646+RuanCarreiroGomes@users.noreply.github.com>
2022-01-17 09:41:35 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -369,22 +369,22 @@ function start() { // Inicio da função start()
 	
 		if (energiaAtual==3) {
 			
-			$("#energia").css("background-image", "url(../imgs/energia3.png)");
+			$("#energia").css("background-image", "url(imgs/energia3.png)");
 		}
 	
 		if (energiaAtual==2) {
 			
-			$("#energia").css("background-image", "url(../imgs/energia2.png)");
+			$("#energia").css("background-image", "url(imgs/energia2.png)");
 		}
 	
 		if (energiaAtual==1) {
 			
-			$("#energia").css("background-image", "url(../imgs/energia1.png)");
+			$("#energia").css("background-image", "url(imgs/energia1.png)");
 		}
 	
 		if (energiaAtual==0) {
 			
-			$("#energia").css("background-image", "url(../imgs/energia0.png)");
+			$("#energia").css("background-image", "url(imgs/energia0.png)");
 			gameOver();
 			//Game Over
 		}
